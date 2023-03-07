4e832366e062f1c962c5920b9d1c2bc8440a2282
mightynovel2023 <121281884+mightynovel2023@users.noreply.github.com>
2023-03-07 10:12:20 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,16 @@
-variable "cidr_block" {
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = " "
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "aws_vpc" {
   description = "The CIDR block for the VPC."
   type        = string
   default     = "10.10.0.0/22"
